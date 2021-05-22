464653d4294a02376a98741e1be2e6c6f7167a27
Jhoan Victor <jhoanvic.melo@gmail.com>
2021-05-22 12:33:37 -0300

    Obtendo um usuário por id + alteração na classe UserRepository

---
+++ src/main/java/com/workshop/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.workshop.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.workshop.services.exception.ObjectNotFoundException;
+
+/*
+ * Classe responsável por tratar possíveis erros na requisição
+ */
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado",
+				e.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(status).body(erro);
+
+	}
+
+}
